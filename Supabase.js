b5448d6d1d86edeaf72793170c3ce42fabe1f15d
PabloCaminito <pablocaminito@gmail.com>
2026-01-03 18:29:47 +0100

    Delete Supabase.js

--- Supabase.js
+++
@@ -1,6 +0,0 @@
-
-// Inicializar Supabase para navegador
-const supabase = supabase.createClient(
-  "https://wvxjfbrgsxakcdltovuh.supabase.co",
-  "sb_publishable_ahEyiel0mSMlvv0RNqC04Q_zT2bSPTZ"
-);
