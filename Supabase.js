c2e90d6cd4e0b3e31e26013f5a6cc0e71c91a748
PabloCaminito <pablocaminito@gmail.com>
2026-01-03 18:26:16 +0100

    Update Supabase.js

--- Supabase.js
+++ Supabase.js
@@ -1,4 +1,6 @@
-const supabase = window.supabase.createClient(
+
+// Inicializar Supabase para navegador
+const supabase = supabase.createClient(
   "https://wvxjfbrgsxakcdltovuh.supabase.co",
   "sb_publishable_ahEyiel0mSMlvv0RNqC04Q_zT2bSPTZ"
 );
